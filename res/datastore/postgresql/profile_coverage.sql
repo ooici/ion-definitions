aa9f4630fc596b1deca4c951a3c0d3f3b0749411
Casey Bryant <casey.bryant@shomo.com>
2014-02-27 04:28:40 -0800

    Added indexes for coverage_spans table.

--- res/datastore/postgresql/profile_coverage.sql
+++ res/datastore/postgresql/profile_coverage.sql
@@ -3,6 +3,14 @@ CREATE TABLE "%(ds)s" (id text PRIMARY KEY, auto_flush_values text, brick_domain
 
 GRANT SELECT, INSERT, UPDATE, DELETE on "%(ds)s" TO ion;
 
-CREATE TABLE "%(ds)s_spans" (span_address text PRIMARY KEY, coverage_id text, spatial_geography geography, time_range tsrange);
+CREATE TABLE "%(ds)s_spans" (span_address text PRIMARY KEY, coverage_id text, spatial_geography geography, vertical_range numrange, time_range tsrange);
+
+CREATE INDEX spans_time_idx on "%(ds)s_spans" (time_range);
+
+CREATE INDEX spans_spatial_idx on "%(ds)s_spans" (spatial_geography);
+
+CREATE INDEX spans_vertical_idx on "%(ds)s_spans" (vertical_range);
+
+CREATE INDEX spans_time_space_idx on "%(ds)s_spans" (time_range, spatial_geography);
 
 GRANT SELECT, INSERT, UPDATE, DELETE on "%(ds)s_spans" TO ion;
